fa29f65f02c70d7bb82a24ab897f8e6f65b0778a
Adrian Brandt <adrian.sleire.brandt@eika.no>
2025-04-13 12:08:26 +0200

    ignored

--- backend/dist/__tests__/setup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockCtx = void 0;
-const jest_mock_extended_1 = require("jest-mock-extended");
-const context_1 = require("@/context");
-exports.mockCtx = (0, context_1.createMockContext)();
-beforeEach(() => {
-    (0, jest_mock_extended_1.mockReset)(exports.mockCtx.prisma);
-});
--- backend/dist/middleware/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contextMiddleware = void 0;
-const context_1 = require("@/context");
-const contextMiddleware = (req, res, next) => {
-    req.context = context_1.appContext;
-    next();
-};
-exports.contextMiddleware = contextMiddleware;
